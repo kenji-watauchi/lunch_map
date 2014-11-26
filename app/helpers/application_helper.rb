a829d5f40f173417d018e7a56b526634c4eb7c66
watauchi <watauchi@APPLE20130730-no-MacBook-Pro.local>
2014-11-26 22:55:05 +0900

    full_titleヘルパー追加したが使うのか・・・？

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  # ページごとの完全なタイトルを返します。
+  def full_title(page_title)
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
+
+  def base_title
+    "Ruby on Rails Tutorial Sample App"
+  end
 end
