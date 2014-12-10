a9ea8c133811b90fb0b677ac669c8152e91d7d97
cotton <watauchi@actindi.net>
2014-12-10 21:26:45 +0900

    Micropostモデル作成

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -1,7 +1,8 @@
 require 'spec_helper'
 
 describe Micropost do
-let(:user) { FactoryGirl.create(:user) }
+
+  let(:user) { FactoryGirl.create(:user) }
   before { @micropost = user.microposts.build(content: "Lorem ipsum") }
 
   subject { @micropost }
@@ -15,6 +16,11 @@ let(:user) { FactoryGirl.create(:user) }
 
   describe "when user_id is not present" do
     before { @micropost.user_id = nil }
+    it { should_not be_valid }
+  end
+
+  describe "with content that is too long" do
+    before { @micropost.content = "a" * 141 }
     it { should_not be_valid }
   end
 end
