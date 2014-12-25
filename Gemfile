b57b8d16ae9de8a5fab100c3d0327ab3a7907743
cotton <watauchi@actindi.net>
2014-12-25 11:39:53 +0900

    Active Recordの構文でマイグレーションし直し

--- Gemfile
+++ Gemfile
@@ -12,7 +12,7 @@ gem 'bootstrap-will_paginate', '0.0.9'
 gem 'paperclip'
 
 group :development, :test do
-  gem 'sqlite3', '1.3.8'
+  gem 'sqlite3', '1.3.8', groups: %w(test development), require: false
   gem 'rspec-rails', '2.13.1'
   gem 'pry'
   gem 'pry-rails'
@@ -42,6 +42,6 @@ group :doc do
 end
 
 group :production do
-  gem 'pg', '0.15.1'
+  gem 'pg', '0.15.1', groups: %w(production), require: false
   gem 'rails_12factor', '0.0.2'
 end
--- db/migrate/20141222063305_create_genres.rb
+++ db/migrate/20141222063305_create_genres.rb
@@ -6,14 +6,14 @@ class CreateGenres < ActiveRecord::Migration
     end
     reversible do |dir|
       dir.up do
-          execute('INSERT INTO genres(name) VALUES("和食")')
-          execute('INSERT INTO genres(name) VALUES("洋食")')
-          execute('INSERT INTO genres(name) VALUES("中華")')
-          execute('INSERT INTO genres(name) VALUES("定食屋")')
-          execute('INSERT INTO genres(name) VALUES("焼き肉")')
-          execute('INSERT INTO genres(name) VALUES("海鮮")')
-          execute('INSERT INTO genres(name) VALUES("丼もの")')
-          execute('INSERT INTO genres(name) VALUES("麺類")')
+          Genre.create(name:"和食")
+          Genre.create(name:"洋食")
+          Genre.create(name:"中華")
+          Genre.create(name:"定食屋")
+          Genre.create(name:"焼き肉")
+          Genre.create(name:"海鮮")
+          Genre.create(name:"丼もの")
+          Genre.create(name:"麺類")
       end
     end
   end
